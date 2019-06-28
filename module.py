ebb3d9492d706881adf46c7caf4b88e5e890e20d
suyoule2003 <suyoule2003@163.com>
2019-06-28 13:03:55 +0800

    Alpha 0.0
    Next I'll add the file explanation function.

---
+++ module.py
@@ -0,0 +1,28 @@
+import random
+
+class Poem:
+    def __init__(self, name, time, poet, content):
+        self.name = name
+        self.time = time
+        self.poet = poet
+        self.content = content
+        self.index1_list = len(content)
+        self.index2_list = []
+        for i in range(len(content)):
+            self.index2_list.append(len(content[i]))
+
+    def random_generate(self):
+        index1 = random.randint(0, len(self.content) - 1)
+        index2 = random.randint(0, self.index2_list[index1] - 1)
+        sentence = ""
+        for i in range(self.index2_list[index2]):
+            if (i == index2):
+                sentence = sentence + "______________"
+            else:
+                sentence = sentence + self.content[index1][i]
+            if (i == self.index2_list[index2] - 1):
+                sentence = sentence + "。"
+            else:
+                sentence = sentence + "，"
+        sentence = sentence + " （" + self.time + "•" + self.poet + " 《" + self.name +"》）"
+        return sentence
